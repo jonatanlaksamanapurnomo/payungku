a097b1fe9cd2d60c1026f8dadd5aedfd88280206
Joo <jonatanlaksamanapurnomo>
2021-01-07 09:45:28 +0700

    push

--- src/component/weathercard.js
+++ src/component/weathercard.js
@@ -1,10 +1,12 @@
-import React,{useState} from 'react';
-import { Collapse, Button, CardBody, Card } from 'reactstrap';
+import React, {useState} from 'react';
+import {Collapse} from 'reactstrap';
 import "./weather.css";
-import { normalizeWeatherCode } from "../utill/filter";
+import {normalizeWeatherCode} from "../utill/filter";
 
 const Weathercard = (props) => {
-    const [open, setOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
     const capitalizeEachWord = (words) => {
         let separateWord = words.toLowerCase().split(' ');
         for (let i = 0; i < separateWord.length; i++) {
@@ -14,7 +16,7 @@ const Weathercard = (props) => {
         return separateWord.join(' ');
     }
 
-    let { description, child } = props.data
+    let {description, child} = props.data
     let weatherList = null;
     let temperatureList = null;
     child.forEach(element => {
@@ -25,68 +27,65 @@ const Weathercard = (props) => {
             if (element.attributes.id === "t") {
                 temperatureList = element.children
             }
-        }
-        catch (err) {
+        } catch (err) {
             console.log("Data tidak valid")
         }
     });
     let currentWeather, currentWeatherIcon, currentWeatherName, currentTemp = null
     try {
-         currentWeather = normalizeWeatherCode(weatherList[0].children[0].value)
-         currentWeatherIcon = currentWeather.url
-         currentWeatherName = currentWeather.name
-         currentTemp = temperatureList[0].children[0].value
-    }
-    catch (err) {
+        currentWeather = normalizeWeatherCode(weatherList[0].children[0].value)
+        currentWeatherIcon = currentWeather.url
+        currentWeatherName = currentWeather.name
+        currentTemp = temperatureList[0].children[0].value
+    } catch (err) {
         console.log("Data tidak valid")
         return null;
     }
 
-    return (<div className="card mx-0  "><span className="icon"><img alt="" className="img-fluid"
-        src={currentWeatherIcon} /></span>
-        <div className="title">
-            <p>{capitalizeEachWord(description)}</p>
-        </div>
-        <div className="temp">{currentTemp}<sup>&deg;</sup>C</div>
-        <div className="row">
-            <div className="col-4">
-                <div className="header">General</div>
-                <div className="value">{currentWeatherName}</div>
+    return (
+        <div onClick={toggle} className="card mx-0  "><span className="icon"><img alt="" className="img-fluid"
+                                                                                  src={currentWeatherIcon}/></span>
+            <div className="title">
+                <p>{capitalizeEachWord(description)}</p>
             </div>
-            <div className="col-4">
-                <div className="header">Air pollution</div>
-                <div className="value">47</div>
+            <div className="temp">{currentTemp}<sup>&deg;</sup>C</div>
+            <div className="row">
+                <div className="col-4">
+                    <div className="header">General</div>
+                    <div className="value">{currentWeatherName}</div>
+                </div>
+                <div className="col-4">
+                    <div className="header">Air pollution</div>
+                    <div className="value">47</div>
+                </div>
+                <div className="col-4">
+                    <div className="header">Fire</div>
+                    <div className="value">No</div>
+                </div>
             </div>
-            <div className="col-4">
-                <div className="header">Fire</div>
-                <div className="value">No</div>
+            <hr></hr>
+            <div className="row mt-1">
+                <div className="col-3">
+                    12
+                </div>
+                <div className="col-3">
+                    12
+                </div>
+                <div className="col-3">
+                    12
+                </div>
+                <div className="col-3">
+                    12
+                </div>
             </div>
-        </div>
-        <hr></hr>
-        <div className="row mt-1" onClick={() => setOpen(!open)}
-        aria-controls="example-collapse-text"
-        aria-expanded={open}>
-            <div className="col-3">
-                12
-            </div>
-            <div className="col-3">
-                12
-            </div>
-            <div className="col-3">
-                12
-            </div>
-            <div className="col-3">
-                12
-            </div>
-        </div>
-        <Collapse in={open}>
-            <div id="example-collapse-text">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-            labore wes anderson cred nesciunt sapiente ea proident.
-            </div>
-        </Collapse>
-    </div>);
+            <Collapse isOpen={isOpen}>
+                <div id="example-collapse-text">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                </div>
+            </Collapse>
+        </div>);
 
 };
 
